b952ca5514534f9c58ec746bfb73cfaf9906ca5f
musense <musense.ad@gmail.com>
2023-08-30 13:54:10 +0800

    remove production console.log

--- next.config.js
+++ next.config.js
@@ -43,7 +43,7 @@ const nextConfig = {
         ]
     },
     compiler: {
-        // removeConsole: process.env.NODE_ENV === 'production',
+        removeConsole: process.env.NODE_ENV === 'production',
     },
 }
 
